4dc074315f9f03c94256f0d8647e4b6f6f35f00c
Nate Montgomery <nathancmontgomery@gmail.com>
2018-07-01 22:04:46 -0700

    first working solution

---
+++ README.md
@@ -0,0 +1,5 @@
+Runs deduplication on provided filename from within `data` directory.
+
+Example Run:
+
+node dedupe.js dedupe "leads.json"
--- ops.js
+++ ops.js
@@ -1,6 +1,14 @@
 var fs = require('fs');
 var _ = require('lodash');
 
+const difference = (object, base) => {
+  return _.transform(object, (result, value, key) => {
+    if (!_.isEqual(value, base[key])) {
+      result[key] = _.isObject(value) && _.isObject(base[key]) ? difference(value, base[key]) : value;
+    }
+  });
+}
+
 const checkDuplicate = (lead, otherLead) => {
   if ((lead._id == otherLead._id) || (lead.email == otherLead.email)) { return true }
   else { return false }
@@ -12,13 +20,65 @@ const dedupeLeads = (filename) => {
       return console.log(err);
     }
 
-    dupeGroups = groupDupes(JSON.parse(data)["leads"]);
-    dedupedLeads = Object.values(dupeGroups);
+    originalLeads = JSON.parse(data)["leads"];
+    dupeGroups = groupDupes(originalLeads);
+    arrayOfDupeGroups = Object.values(dupeGroups);
 
     finalLeads = {};
-    finalLeads["leads"] = _.compact(dedupedLeads.map(leads => leads.pop()));
 
-    console.log(finalLeads)
+    for (var dupeGroup of arrayOfDupeGroups) {
+      for (var lead of dupeGroup) {
+        latestLeads = [];
+
+        arrayOfDupeGroups.map(
+          function(otherDupeGroup) {
+            anyMatchingDupesBetweenGroups = !_.isEmpty(_.compact(otherDupeGroup.map(otherLead => checkDuplicate(lead, otherLead))));
+
+            if (anyMatchingDupesBetweenGroups) {
+              latestDupe = dupeGroup.slice(-1).pop();
+              latestOtherDupe = otherDupeGroup.slice(-1).pop();
+
+              latestDupeDate = new Date(latestDupe.entryDate);
+              latestOtherDupeDate = new Date(latestOtherDupe.entryDate);
+
+              if (latestDupeDate <= latestOtherDupeDate) {
+                latestLeads.push(latestOtherDupe);
+              } else {
+                latestLeads.push(latestDupe);
+              }
+            }
+          }
+        );
+
+        finalLead = _.orderBy(latestLeads, function(lead) { return new Date(lead.entryDate); }, "asc").pop();
+
+        if (finalLeads[`${lead._id} at ${lead.email}`]) {
+          finalLeads[`${lead._id} at ${lead.email}`].push(finalLead);
+        } else {
+          finalLeads[`${lead._id} at ${lead.email}`] = [finalLead];
+        }
+      }
+    }
+
+    output = originalLeads.map(function(lead) {
+      console.log("MAPPING: \n", lead);
+
+      mappedLead = finalLeads[`${lead._id} at ${lead.email}`].slice(-1).pop();
+      console.log("TO: \n", mappedLead);
+
+      changeset = difference(lead, mappedLead);
+      console.log("CHANGES: \n", );
+
+      Object.keys(changeset).map(function(key) {
+        console.log(key, " from: ", lead[key]," to: ", mappedLead[key]);
+      });
+
+      console.log("\n\n");
+
+      return finalLeads[`${lead._id} at ${lead.email}`].slice(-1).pop();
+    });
+
+    console.log("OUTPUT: \n", _.uniqWith(output, _.isEqual));
   });
 }
 
@@ -31,34 +91,7 @@ const groupDupes = (leads) => {
 
 const detectDupes = (lead, leads, currentDupes) => {
   newDupes = leads.filter(otherLead => checkDuplicate(lead, otherLead));
-  orderedDupes = _.orderBy(newDupes, function(lead) { return new Date(lead.entryDate); }, "asc");
-
-  // This nested search cross references existing duplication groups against
-  // the new duplication group we would create.
-  for (var existingDupeGroup of currentDupes) {
-    for (var newLead of orderedDupes) {
-      matchingDupes = existingDupeGroup.filter(oldLead => checkDuplicate(oldLead, newLead));
-
-      if (!_.isEmpty(matchingDupes)) {
-        latestNewDupe = new Date(orderedDupes.pop());
-        latestExistingDupe = new Date(matchingDupes.pop());
-
-        if (latestExistingDupe.entryDate > latestNewDupe.entryDate) {
-          console.log("Mapping Lead:");
-          console.log(newLead);
-          console.log("To:");
-          console.log(matchingDupes.pop());
-          console.log("\n");
-        } else {
-          existingDupeGroup.push(latestNewDupe);
-        }
-
-        return [];
-      }
-    }
-  }
-
-  return orderedDupes;
+  return _.orderBy(newDupes, function(lead) { return new Date(lead.entryDate); }, "asc");
 }
 
 module.exports = { dedupeLeads }
